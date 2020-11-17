b4c91b55f2c84a13e3e4f1233639bbc2d9b22653
1612491042-Suchana <73894479+Sanjida1612491042@users.noreply.github.com>
2020-11-18 00:00:26 +0600

    Files Added

---
+++ Homework Submission/Homework 1/2.asm
@@ -0,0 +1,18 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+MOV AX, 02
+ADD AX, 04
+ADD AX, 06
+ADD AX, 08
+ADD AX, 10
+
+MOV CX,AX
+ret
+
+
+
+
